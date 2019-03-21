e2c860afec4d2fecc38bc26d8fb03d28dad740a8
Alexander <banshchikov.alex@outlook.com>
2019-03-21 22:57:43 +0700

    Added connection string with DataBase.

--- CalculateDescriptor.py
+++ CalculateDescriptor.py
@@ -1,10 +1,24 @@
 import dlib
 from skimage import io
 import json
+from sqlalchemy import create_engine
 
-sp = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
-facerec = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
+sp = dlib.shape_predictor('RecognitionFiles/shape_predictor_68_face_landmarks.dat')
+facerec = dlib.face_recognition_model_v1('RecognitionFiles/dlib_face_recognition_resnet_model_v1.dat')
 detector = dlib.get_frontal_face_detector()
+
+connection_string = "postgresql+psycopg2://admin:password@localhost/student_control"
+
+engine = create_engine(connection_string)
+
+group = '8И5А'
+
+query = ("SELECT s.name, s.descriptor "
+         "FROM student s "
+         "JOIN studygroup gr ON (gr.Id = s.GroupId) "
+         "WHERE gr.Name = '{0}'").format(group)
+
+
 img = io.imread("/home/banshchikovalex/Изображения/UIRS/!Kondratieva.jpg") # Считывание дескриптора с фото
 
 win1 = dlib.image_window()
@@ -24,9 +38,6 @@ descriptor = str(face_descriptor1) # Запись в JSON
 name = ''
 
 dict = {'Name': name, 'Descriptor': descriptor}
-
-# while True:
-#    continue
 
 with open('data.json', 'a') as f:
     myobjectJson = json.dumps(dict)

0f9fbcf691caae1d95bdba9b0e047e3a4ad13e19
Alexander <banshchikov.alex@outlook.com>
2019-04-14 11:51:05 +0700

    Refactoring.

--- Recognition.py
+++ Recognition.py
@@ -5,18 +5,20 @@ from tkinter import *
 import cv2
 import json
 
-timeListReport = []
+time_list_report = []
 
-def GetReport(array):
+
+def get_report(array, time_list_report):
     for item in array:
         flag = False
-        for human in timeListReport:
+        for human in time_list_report:
             if item['name'] == human['name']:
                 human['time2'] = item['time']
                 flag = True
                 break
         if flag is False:
-            timeListReport.append({'name': item['name'], 'time1': item['time'], 'time2': item['time']})
+            time_list_report.append({'name': item['name'], 'time1': item['time'], 'time2': item['time']})
+
 
 faceCascade = cv2.CascadeClassifier('RecognitionFiles/haarcascade_frontalface_default.xml')
 
@@ -28,7 +30,7 @@ sp = dlib.shape_predictor('RecognitionFiles/shape_predictor_68_face_landmarks.da
 facerec = dlib.face_recognition_model_v1('RecognitionFiles/dlib_face_recognition_resnet_model_v1.dat')
 detector = dlib.get_frontal_face_detector()
 
-with open('data.json', 'r') as f:
+with open('student_data.json', 'r') as f:
     data = json.loads(f.read())
     countDescriptors = len(data)
 
@@ -52,18 +54,20 @@ while True:
 
         i = 0
         descriptorsRaw = []
-        currentDescriptor = ()
+
         while i < countDescriptors:
             descriptorsRaw.append(data[i]['Descriptor'])
             currentDescriptor = tuple(float(item) for item in descriptorsRaw[i].split('\n'))
             try:
                 a = distance.euclidean(currentDescriptor, face_descriptor2)
-                if a < 0.5:
-                    timeListBuffer.append({'name': data[i]['Name'], 'time': datetime.strftime(datetime.now(), "%H:%M:%S")})
+                if a < 0.55:
+                    timeListBuffer.append({'name': data[i]['Name'],
+                                           'time': datetime.strftime(datetime.now(),
+                                           "%H:%M:%S")})
                     break
                 i += 1
-            except ValueError:
-                continue
+            except NameError:
+                break
 
         cv2.rectangle(img2, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
@@ -75,13 +79,13 @@ while True:
     if k == 27:  # press 'ESC' to quit
         break
     if k == 13:
-        GetReport(timeListBuffer)
-        root = Tk()
+        get_report(timeListBuffer, time_list_report)
+        root = Tk()                           # Добавить форму со списком студентов, названием занятия, преподавателем
         root.title("Отчет")
         text = Text(width=70, height=25)
         text.grid(columnspan=3)
 
-        for item in timeListReport:
+        for item in time_list_report:
             string = str(item['name']) + " " + str(item['time1']) + ' ' + str(item['time2']) + '\n'
             text.insert(1.0, string)
 
@@ -89,21 +93,3 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-
-# win1.wait_until_closed()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
